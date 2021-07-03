9fefdf19768b8e954d578f881d938baa7d42290e
freshield <yangyufresh@163.com>
2021-07-03 16:03:16 +0800

    done the surveing

--- chp2_surveing/pages/index/index.js
+++ chp2_surveing/pages/index/index.js
@@ -5,15 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name: '张三',
+    gender: [
+      {name: '男', value: '0', checked: true},
+      {name: '女', value: '1', checked: false}
+    ],
+    skills: [
+      {name: 'HTML', value: 'html', checked: true},
+      {name: 'CSS', value: 'css', checked: true},
+      {name: 'JavaScript', value: 'js', checked: false},
+      {name: 'Photoshop', value: 'ps', checked: false}
+    ],
+    opinion: '测试'
   },
   submitting: function(e) {
     wx.request({
       method: 'post',
       url: 'http://127.0.0.1:9999/',
       data: e.detail.value,
-      success: function(res) {
-        console.log(res)
+      success: res => {
+        this.setData(res.data.data)
       }
     })
   },
@@ -22,7 +33,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData(this.data)
+    console.log(this)
   },
 
   /**
