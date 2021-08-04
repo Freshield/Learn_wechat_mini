2cae08ca877980f82b59b14a471c0e0dfac06585
freshield <yangyufresh@163.com>
2021-08-04 16:43:18 +0800

    done chp3

--- chp3/a1_music/pages/index/index.js
+++ chp3/a1_music/pages/index/index.js
@@ -1,13 +1,11 @@
 // pages/index/index.js
+var music_src = 'http://m10.music.126.net/20210727201925/dbad7ed7355ecb235df7d30cf3ed2abd/ymusic/6ce6/b2c1/9fac/ed75227c13e71a49cb885007385d08c2.mp3'
 Page({
-
   /**
    * 页面的初始数据
    */
-  
   data: {
     item: 0,
-    _src: 'http://m10.music.126.net/20210727201925/dbad7ed7355ecb235df7d30cf3ed2abd/ymusic/6ce6/b2c1/9fac/ed75227c13e71a49cb885007385d08c2.mp3',
     state: 'paused',
     playIndex: 0,
     play: {
@@ -18,14 +16,15 @@ Page({
       singer: '',
       coverImgUrl: '/images/cover.jpg'
     },
+    
     playlist: [{
-      id: 1, title: '钢琴协奏曲', singer: '肖邦', src: Page._src, coverImgUrl: '/images/cover.jpg'
+      id: 1, title: '钢琴协奏曲', singer: '肖邦', src: music_src, coverImgUrl: '/images/cover.jpg'
     }, {
-      id: 2, title: '奏鸣曲', singer: '莫扎特', src: Page._src, coverImgUrl: '/images/cover.jpg'
+      id: 2, title: '奏鸣曲', singer: '莫扎特', src: music_src, coverImgUrl: '/images/cover.jpg'
     }, {
-      id: 3, title: '欢乐颂', singer: '贝多芬', src: Page._src, coverImgUrl: '/images/cover.jpg'
+      id: 3, title: '欢乐颂', singer: '贝多芬', src: music_src, coverImgUrl: '/images/cover.jpg'
     }, {
-      id: 4, title: '爱之梦', singer: '李斯特', src: Page._src, coverImgUrl: '/images/cover.jpg'
+      id: 4, title: '爱之梦', singer: '李斯特', src: music_src, coverImgUrl: '/images/cover.jpg'
     }]
   },
   changeItem: function(e) {
@@ -52,11 +51,35 @@ Page({
   audioCtx: null,
   onReady: function () {
     this.audioCtx = wx.createInnerAudioContext()
+    var that = this
+    this.audioCtx.onError(function() {
+      console.log('播放失败：' + that.audioCtx.src)
+    })
+    // 播放完了自动换下一曲
+    this.audioCtx.onEnded(function() {
+      that.next()
+    })
+    // 自动更新播放进度
+    this.audioCtx.onPlay(function() {})
+    this.audioCtx.onTimeUpdate(function() {
+      that.setData({
+        'play.duration': formatTime(that.audioCtx.duration),
+        'play.currentTime': formatTime(that.audioCtx.currentTime),
+        'play.percent': that.audioCtx.currentTime / that.audioCtx.duration * 100
+      })
+    })
     this.setMusic(0)
+    function formatTime(time) {
+      var minute = Math.floor(time / 60) % 60
+      var second = Math.floor(time) % 60
+      return (minute < 10 ? '0' + minute : minute) + ':' +
+      (second < 10 ? '0' + second : second)
+    }
   },
   setMusic: function(index) {
     var music = this.data.playlist[index]
-    this.audioCtx.src = this.data._src
+    this.audioCtx.src = music.src
+    this.audioCtx.seek(0)
     this.setData({
       playIndex: index,
       'play.title': music.title,
@@ -82,7 +105,20 @@ Page({
       this.play()
     }
   },
-
+  sliderChange: function (e) {
+    var second = e.detail.value * this.audioCtx.duration / 100
+    this.audioCtx.seek(second)
+  },
+  changePage: function (e) {
+    var page = e.target.dataset.page
+    this.setData({
+      item: page
+    })
+  },
+  change: function(e) {
+    var index = e.currentTarget.dataset.index
+    this.setMusic(index)
+  },
   /**
    * 生命周期函数--监听页面显示
    */
